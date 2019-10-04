d95c2503ca8a9092e21b44338fdac2896ddd740d
tom soukup <tosinek@gmail.com>
2019-10-04 15:35:31 +0200

    dynamoDB access for getItem

--- api.js
+++ api.js
@@ -1,6 +1,27 @@
 const http = require('http')
 var AWS = require('aws-sdk')
-var dynamo = new AWS.DynamoDB.DocumentClient()
+var documentClient = new AWS.DynamoDB.DocumentClient({ region: 'eu-central-1' })
+
+class DB {
+  get(key, value, table) {
+    if (!table) throw 'table needed'
+    if (typeof key !== 'string') throw `key was not string and was ${JSON.stringify(key)} on table ${table}`
+    if (typeof value !== 'string') throw `value was not string and was ${JSON.stringify(value)} on table ${table}`
+    return new Promise((resolve, reject) => {
+      const params = {
+        TableName: table,
+        Key: { [key]: value }
+      }
+      documentClient.get(params, (err, data) => {
+        if (err) {
+          console.log(`There was an error fetching the data for ${key} ${value} on table ${table}`, err)
+          return reject(err)
+        }
+        return resolve(data)
+      })
+    })
+  }
+}
 
 exports.handler = async (event, context) => {
   const { from, to, amount } = event.queryStringParameters
@@ -12,7 +33,8 @@ exports.handler = async (event, context) => {
   if (from !== 'EUR') return respond(400, 'free plan allows only conversion from EUR')
 
   // call fixer API
-  const fixerReply = await fetch('http://data.fixer.io/api/latest?access_key=11e71290875f5b9fb0c096eaa6735c93&format=1').catch(err => err)
+  // todo make the access key a part of the env vars
+  const fixerReply = await fetch('http://data.fixer.io/api/latest?access_key=11e71290875f5b9fb0c096eaa6735c93&format=1').catch(e => e)
   console.log('fixer says:', typeof fixerReply, fixerReply)
 
   const rates = JSON.parse(fixerReply)
@@ -26,6 +48,17 @@ exports.handler = async (event, context) => {
   console.log('exchRate', exchRate)
 
   // store data to DynamoDB
+  // const DB = require('./dynamo')
+  const Dynamo = new DB()
+  const dynamoData = await Dynamo.get('id', 'conversionStats', 'currencyConversionStats').catch(e => e)
+  console.log('dynamo data', dynamoData)
+
+  // conversionCount: 2,
+  // conversionAmountInUsd: 350,
+  // destinationCurrencies: {
+  //   CZK: 2,
+  //   EUR: 5,
+  // }
 
 
   return respond(200, result)
